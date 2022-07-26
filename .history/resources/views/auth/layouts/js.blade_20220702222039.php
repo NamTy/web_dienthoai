aca32c26d99d36ee740ffd33e190df28703c4d9a
Nam Ty <trinhhongnam171020201220@gmail.com>
2022-07-27 00:48:00 +0700

    updateAdmin

--- .history/resources/views/auth/layouts/js.blade_20220702222039.php
+++
@@ -1,8 +0,0 @@
-<script src="{{ asset('auth/js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{ asset('auth/js/bootstrap.min.js')}}"></script>
-    <script src="{{ asset('auth/js/jquery.nice-select.min.js')}}"></script>
-    <script src="{{ asset('auth/js/jquery-ui.min.js')}}"></script>
-    <script src="{{ asset('auth/js/jquery.slicknav.js')}}"></script>
-    <script src="{{ asset('auth/js/mixitup.min.js')}}"></script>
-    <script src="{{ asset('auth/js/owl.carousel.min.js')}}"></script>
-    <script src="{{ asset('auth/js/main.js')}}"></script>
--- .history/resources/views/client/layouts/js.blade_20220711222232.php
+++
@@ -1,8 +0,0 @@
-    <script src="{{asset('client/js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{asset('client/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('client/js/jquery.nice-select.min.js')}}"></script>
-    <script src="{{asset('client/js/jquery-ui.min.js')}}"></script>
-    <script src="{{asset('client/js/jquery.slicknav.js')}}"></script>
-    <script src="{{asset('client/js/mixitup.min.js')}}"></script>
-    <script src="{{asset('client/js/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('client/js/main.js')}}"></script>
